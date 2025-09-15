535bebab7cf70a6b3d2d69b0c2b0a49518ea62dc
JFMS <jfmsproyect@gmail.com>
2025-09-15 20:32:34 -0300

    Delete Proyecto directory

--- Proyecto/Ayuda/login/login_back.php
+++
@@ -1,7 +0,0 @@
- <?php
-   // session_start();
-    // en este ejemplo solo se trabaja con sesiones
-    // por lo tanto no se busca al usuario en una base de datos
-   //$_SESSION['usr_name'] = $_POST['usr_name'];
-   //header('location: ../index.php');
-?>

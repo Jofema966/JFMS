a09df9f211de33b04967ccb871976b5dc0c91601
JFMS <jfmsproyect@gmail.com>
2025-11-18 00:05:09 -0300

    Delete Proyecto JFMS directory

--- Proyecto JFMS/assets/js/common.js
+++
@@ -1,5 +0,0 @@
-
-export async function postJSON(url, data){
-  const r = await fetch(url, {method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify(data||{})});
-  return r.json();
-}
